e211f33cda767d564904344b81715ed2ab626070
Flipaduz <masterjugon@gmail.com>
2025-03-06 12:27:36 +0100

    Inicio Test ClubDeportivo
    Implementacion inacabada de test que comprueba que los valores son validos al añadir una nueva actividad al club deportivo.

--- src/test/java/clubdeportivo/ClubDeportivoTest.java
+++ src/test/java/clubdeportivo/ClubDeportivoTest.java
@@ -1,3 +1,22 @@
+package clubdeportivo;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 public class ClubDeportivoTest {
     
+    @Test
+    public void ValoresValidosAlanyadirActividad() throws ClubException{
+        //Arrange
+        ClubDeportivo cd = new ClubDeportivo("Malaga");
+        //String[] datos ={"str1","str2","10","20","1.34"};
+        String[] datos ={"str1","str2","str3","str4","str5"};
+        //Act
+        assertThrows(ClubException.class,() ->{cd.anyadirActividad(datos);});
+
+        //Assert
+        //assertEquals(exception, "ERROR: formato de número incorrecto");
+    }
+
 }
